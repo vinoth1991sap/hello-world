ba1f1e8cd296caf69d0fad931bc1b0f32ef39b30
vinoth1991sap <67548830+vinoth1991sap@users.noreply.github.com>
2020-07-08 07:00:42 +0530

    Update HelloWorld.java
    changes are done

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to jenkins journey");
+		System.out.println("welcome to jenkins journey1");
 	}
 
 }
